c4f321ac79ed0e5b5ca6d4228be545ff66b9bc7d
Seongeun-Yu <oksung0804@gmail.com>
2024-11-19 00:11:29 +0900

    Dynamic chart using Parameter CDS View

--- sales_chart/webapp/controller/SalesView.controller.js
+++ sales_chart/webapp/controller/SalesView.controller.js
@@ -2,20 +2,25 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Sorter",
     "sap/viz/ui5/format/ChartFormatter",
-    "sap/viz/ui5/api/env/Format"
+    "sap/ui/core/format/DateFormat"
 ],
-function (Controller, Sorter, ChartFormatter, Format) {
+function (Controller, Sorter, ChartFormatter, DateFormat) {
     "use strict";
 
     return Controller.extend("cl3.syncyoung.sd.saleschart.saleschart.controller.SalesView", {
         onInit: function () {
+            var sPath = "SalesPerChannelSet(p_start_date='19000101',p_end_date='99990131')/Set";
+            this.getView().bindElement({
+                path: sPath,
+                model: ""
+            });
 
             // (1) Get oData for sales per material
             var oMaterialModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC302SDCDS0002_CDS/"); // ODataModel 초기화
 
             oMaterialModel.read("/SalesPerMaterialSet", {
                 success: function(oData) {
-                    console.log("데이터 읽기 성공:", oData);
+                    console.log("데이터 읽기 성공:", oData.results);
                 },
                 error: function(oError) {
                     console.error("오류 발생:", oError);
@@ -58,7 +63,29 @@ function (Controller, Sorter, ChartFormatter, Format) {
             var oMaterialChart = this.getView().byId("Material");
             var oMaterialPopOver = this.getView().byId("MaterialPopOver");
             oMaterialPopOver.connect(oMaterialChart.getVizUid());
-            oMaterialPopOver.setFormatString(ChartFormatter.DefaultPattern.STANDARDCURRENCY.STANDARDFLOAT);
+            oMaterialPopOver.setFormatString(ChartFormatter.DefaultPattern.STANDARDCURRENCY.STANDARDFLOAT); 
+        },
+
+        onPress : function(){
+            // For Date Formatting
+            var oDateFormat = DateFormat.getDateInstance({ pattern: "yyyyMMdd" });
+
+            // Get Date Input
+            var startDate = oDateFormat.format(this.byId("dateInput").getDateValue());
+            var endDate = oDateFormat.format(this.byId("dateInput").getSecondDateValue());
+
+            // Make Path
+            var sMaterialPath = "material>/SalesPerMaterialSet(p_start_date='" + startDate + "',p_end_date='" + endDate + "')/Set";
+            var sBPPath = "bp>/SalesPerBPSet(p_start_date='" + startDate + "',p_end_date='" + endDate + "')/Set";
+
+            // Get Chart Element
+            var oMaterialChart = this.byId("MaterialChart");
+            var oBPChart = this.byId("BPChart");
+
+            // Change data binding
+            oMaterialChart.bindAggregation("data", { path: sMaterialPath });
+            oBPChart.bindAggregation("data", { path: sBPPath });
+
         }
     });
 });
