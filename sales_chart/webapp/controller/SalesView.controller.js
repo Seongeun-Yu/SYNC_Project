6570749992566e455d855d6a0e29f8c719178bce
Seongeun-Yu <oksung0804@gmail.com>
2024-11-18 20:32:03 +0900

    Add PopOver to Chart

--- sales_chart/webapp/controller/SalesView.controller.js
+++ sales_chart/webapp/controller/SalesView.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/Sorter"
+    "sap/ui/model/Sorter",
+    "sap/viz/ui5/format/ChartFormatter",
+    "sap/viz/ui5/api/env/Format"
 ],
-function (Controller, Sorter) {
+function (Controller, Sorter, ChartFormatter, Format) {
     "use strict";
 
     return Controller.extend("cl3.syncyoung.sd.saleschart.saleschart.controller.SalesView", {
@@ -41,6 +43,22 @@ function (Controller, Sorter) {
             // (4) Sorting
             this.byId("BPChart").getBinding("data").sort(new Sorter("netwr", "true"));          // BP별 매출 차트 : 매출 내림차순 정렬
             this.byId("MaterialChart").getBinding("data").sort(new Sorter("netwr", "true"));    // 자재별 매출 차트 : 매출 내림차순 정렬
+
+            // (5) PopOver
+            var oChannelChart = this.getView().byId("Channel");
+            var oChannelPopOver = this.getView().byId("ChannelPopOver");
+            oChannelPopOver.connect(oChannelChart.getVizUid());
+            oChannelPopOver.setFormatString(ChartFormatter.DefaultPattern.STANDARDCURRENCY.STANDARDFLOAT);
+
+            var oBPChart = this.getView().byId("BP");
+            var oBPPopOver = this.getView().byId("BPPopOver");
+            oBPPopOver.connect(oBPChart.getVizUid());
+            oBPPopOver.setFormatString(ChartFormatter.DefaultPattern.STANDARDCURRENCY.STANDARDFLOAT);
+
+            var oMaterialChart = this.getView().byId("Material");
+            var oMaterialPopOver = this.getView().byId("MaterialPopOver");
+            oMaterialPopOver.connect(oMaterialChart.getVizUid());
+            oMaterialPopOver.setFormatString(ChartFormatter.DefaultPattern.STANDARDCURRENCY.STANDARDFLOAT);
         }
     });
 });
