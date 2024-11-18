1011c1ad702ee0532131a5f2693ec889169794d0
Seongeun-Yu <oksung0804@gmail.com>
2024-11-19 01:38:40 +0900

    Set DataRangeModel

--- sales_chart/webapp/controller/SalesView.controller.js
+++ sales_chart/webapp/controller/SalesView.controller.js
@@ -2,17 +2,21 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Sorter",
     "sap/viz/ui5/format/ChartFormatter",
-    "sap/ui/core/format/DateFormat"
+    "sap/ui/core/format/DateFormat",
 ],
 function (Controller, Sorter, ChartFormatter, DateFormat) {
     "use strict";
 
     return Controller.extend("cl3.syncyoung.sd.saleschart.saleschart.controller.SalesView", {
         onInit: function () {
-            var sPath = "SalesPerChannelSet(p_start_date='19000101',p_end_date='99990131')/Set";
-            this.getView().bindElement({
-                path: sPath,
-                model: ""
+            var oDateRangeModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC302SDCDS0010_CDS");
+            oDateRangeModel.read("/DateRangeSet", {
+                success: function(oData) {
+                    console.log("DateRange 데이터 읽기 성공: ", oData.results);     
+                },
+                error: function(oError){
+                    console.log("DateRange 오류 발생: ", oError);
+                }
             });
 
             // (1) Get oData for sales per material
@@ -20,10 +24,10 @@ function (Controller, Sorter, ChartFormatter, DateFormat) {
 
             oMaterialModel.read("/SalesPerMaterialSet", {
                 success: function(oData) {
-                    console.log("데이터 읽기 성공:", oData.results);
+                    console.log("Material 데이터 읽기 성공:", oData.results);
                 },
                 error: function(oError) {
-                    console.error("오류 발생:", oError);
+                    console.error("Material 오류 발생:", oError);
                 }
             }); 
 
@@ -32,18 +36,17 @@ function (Controller, Sorter, ChartFormatter, DateFormat) {
 
             oBPModel.read("/SalesPerBPSet", {
                 success: function(oData) {
-                    console.log("데이터 읽기 성공:", oData);
+                    console.log("BP 데이터 읽기 성공:", oData.results);
                     
                 },
                 error: function(oError) {
-                    console.error("오류 발생:", oError);
+                    console.error("BP 오류 발생:", oError);
                 }
             });
 
             // (3) Set oDatas to each models
             this.getView().setModel(oMaterialModel, "material");
             this.getView().setModel(oBPModel, "bp")
-
 
             // (4) Sorting
             this.byId("BPChart").getBinding("data").sort(new Sorter("netwr", "true"));          // BP별 매출 차트 : 매출 내림차순 정렬
