00bfeec4d54ca583f7193ce74034661480c41d74
Seongeun-Yu <oksung0804@gmail.com>
2024-11-20 00:26:41 +0900

    Improve UI

--- sales_chart/webapp/controller/SalesView.controller.js
+++ sales_chart/webapp/controller/SalesView.controller.js
@@ -2,25 +2,13 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Sorter",
     "sap/viz/ui5/format/ChartFormatter",
-    "sap/ui/core/format/DateFormat",
-    "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/core/format/DateFormat"
 ],
-function (Controller, Sorter, ChartFormatter, DateFormat, Filter, FilterOperator) {
+function (Controller, Sorter, ChartFormatter, DateFormat) {
     "use strict";
 
     return Controller.extend("cl3.syncyoung.sd.saleschart.saleschart.controller.SalesView", {
         onInit: function () {
-            // var oDateRangeModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC302SDCDS0010_CDS");
-            // oDateRangeModel.read("/DateRangeSet", {
-            //     success: function(oData) {
-            //         console.log("DateRange 데이터 읽기 성공: ", oData.results);     
-            //     },
-            //     error: function(oError){
-            //         console.log("DateRange 오류 발생: ", oError);
-            //     }
-            // });
-            // (1) Get oData for sales per material
             var oMaterialModel = new sap.ui.model.odata.v2.ODataModel("/sap/opu/odata/sap/ZC302SDCDS0002_CDS/"); // ODataModel 초기화
             oMaterialModel.read("/SalesPerMaterialSet(p_start_date='19000101',p_end_date='99990131')/Set", {
                 success: function(oData) {
@@ -46,18 +34,55 @@ function (Controller, Sorter, ChartFormatter, DateFormat, Filter, FilterOperator
             // (3) Set oDatas to each models
             this.getView().setModel(oMaterialModel, "material");
             this.getView().setModel(oBPModel, "bp");
-            // this.getView().setModel(oDateRangeModel, "dateRange");
-
-            // var minDate = oDateRangeModel.getProperty("/DateRangeSet(mindate='20231101',maxdate='20241201')/mindate");
-            // console.log(minDate);
-            // var minDate = this.byId.getProperty("/DateRangeSet(mindate='20231101',maxdate='20241201')/mindate")
-
 
             // (4) Sorting
             this.byId("BPChart").getBinding("data").sort(new Sorter("netwr", "true"));          // BP별 매출 차트 : 매출 내림차순 정렬
             this.byId("MaterialChart").getBinding("data").sort(new Sorter("netwr", "true"));    // 자재별 매출 차트 : 매출 내림차순 정렬
 
-            // (5) PopOver
+            // (5) Filtering
+            // Get VizFrame
+            var oVizFrame = this.getView().byId("BP");
+
+            // // Set Filter
+            // var aFilters = [];
+            // aFilters.push(new Filter("bp>waers", FilterOperator.EQ, "USD"));
+            
+            // var oDataSet = new FlattenedDataset
+            // (
+            //     {
+            //         dimensions:
+            //         [
+            //             {
+            //                 name:"BP",
+            //                 value: "{bp>cname}"
+            //             },
+            //             {
+            //                 name:"통화",
+            //                 value: "{bp>waers}"
+            //             }
+            //         ],
+
+            //         measures:
+            //         [
+            //             {
+            //                 name:"매출",
+            //                 value:"{bp>netwr}"
+            //             }
+            //         ],
+
+            //         data:
+            //         {
+            //             path:"bp>/SalesPerBPSet",
+            //             filters: aFilters
+            //         }
+            //     }
+            // );
+
+            // oVizFrame.removeAllFeeds();
+            // oVizFrame.setDataset(oDataSet);
+
+
+            // (6) PopOver
             var oChannelChart = this.getView().byId("Channel");
             var oChannelPopOver = this.getView().byId("ChannelPopOver");
             oChannelPopOver.connect(oChannelChart.getVizUid());
@@ -98,7 +123,7 @@ function (Controller, Sorter, ChartFormatter, DateFormat, Filter, FilterOperator
                 sMaterialPath = "material>/SalesPerMaterialSet(p_start_date='19000101',p_end_date='99991231')/Set";
                 sBPPath = "bp>/SalesPerBPSet(p_start_date='19000101',p_end_date='99991231')/Set";
             }
-            
+
             // Get Chart Element
             var oChannelChart = this.byId("ChannelChart");
             var oMaterialChart = this.byId("MaterialChart");
@@ -108,6 +133,39 @@ function (Controller, Sorter, ChartFormatter, DateFormat, Filter, FilterOperator
             oChannelChart.bindAggregation("data", { path: sChannelPath });
             oMaterialChart.bindAggregation("data", { path: sMaterialPath });
             oBPChart.bindAggregation("data", { path: sBPPath });
+        },
+
+        onDatePress : function(option){
+            var oDatePicker = this.byId("dateInput");
+            var oToday = new Date();
+            var oStartDate, oEndDate;
+
+            switch(option){
+                case 'today' : 
+                    oStartDate = oEndDate = oToday;
+                    break;
+                case 'week' : 
+                    // First day of this week(Monday)
+                    oStartDate = new Date(oToday);
+                    oStartDate.setDate(oToday.getDate() - oToday.getDay() + 1);
+                    // Last day of this week(Sunday)
+                    oEndDate = new Date(oToday);
+                    oEndDate.setDate(oStartDate.getDate() + 6);
+                    break;
+                case 'month' : 
+                    // First day of this month
+                    oStartDate = new Date(oToday.getFullYear(), oToday.getMonth(), 1);
+                    // Last day of this month
+                    oEndDate = new Date(oToday.getFullYear(), oToday.getMonth() + 1, 0);
+                    break;
+                case 'year' : 
+                    oStartDate = new Date(oToday.getFullYear(), 0, 1);
+                    oEndDate = new Date(oToday.getFullYear(), 11, 31);
+                    break;
+            }
+
+            oDatePicker.setDateValue(oStartDate);
+            oDatePicker.setSecondDateValue(oEndDate);
         }
     });
 });
